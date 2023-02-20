40f5e80ce4b65e4d34edc30b0498b1fd69617a99
unknown <vladfedko35@gmail.com>
2023-02-20 13:16:52 +0200

    KP1

--- KP1/KP1/Algorithm.cs
+++ KP1/KP1/Algorithm.cs
@@ -1,9 +1,18 @@
-﻿using System;
+﻿//using System;
 
-namespace KP1
-{
-    class Algorithm
-    {
 
-    }
-}
+//namespace KP1;
+//class Algorithm
+//{
+//    private int[] array = new int [100];
+//    Random random = new Random();   
+
+//    public void Input()
+//    {
+//        for (int i = 0; i < array.Length; i++)
+//        {
+//            array[i] =Convert.ToInt32(Console.ReadLine());
+
+//        }
+//    }
+//}
--- KP1/KP1/Program.cs
+++ KP1/KP1/Program.cs
@@ -1,11 +1,103 @@
-﻿using System;
+﻿using System.Diagnostics;
 
-namespace KP1;
-
-class Program
+class TA1
 {
-    static void Main(string[] args)
+    static void Main()
     {
-
+        string locate = Environment.CurrentDirectory;
+        locate = locate.Remove(locate.Length - 16);
+        int flag = 0;
+        try
+        {
+            Console.WriteLine("1.Ввести з консолі");
+            Console.WriteLine("2.Зчитати з файлу");
+            flag = Convert.ToInt32(Console.ReadLine());
+            Input(locate + "in.txt", out int n, out int[] array, flag);
+            var timer = new Stopwatch();
+            timer.Start();
+            SwapMinAndMax(n, array);
+            timer.Stop();
+            Output(locate + "out", array, flag);
+            Console.WriteLine("Час виконання:" + timer.Elapsed);
+            Console.ReadKey();
+        }
+        catch (Exception)
+        {
+            if (flag == 2)
+                File.WriteAllText(locate + "out", "Помилка введених даних");
+            else
+                Console.WriteLine("Помилка введених даних");
+        }
     }
+    static void Input(in string locate, out int n, out int[] array, in int flag)
+    {
+        string[] lines = new string[2];
+        if (flag == 2)
+        {
+            lines = File.ReadAllLines(locate);
+            n = Convert.ToInt32(lines[0]);
+        }
+        else
+        {
+            Console.Write("n:");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Масив:");
+            lines[1] = Console.ReadLine()!;
+        }
+        array = new int[n];
+        if (lines[1] == "random")
+        {
+            var rand = new Random(Guid.NewGuid().GetHashCode());
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = rand.Next(-100,100);
+            }
+            lines[1] = string.Join(" ", array);
+            if (flag == 2)
+                File.WriteAllLines(locate, lines);
+            else
+            {
+                Console.Write("Рандомний масив:");
+                Console.WriteLine(lines[1]);
+            }
+        }
+        else
+        {
+            string[] data = lines[1].Split();
+            for (int i = 0; i < data.Length; i++)
+            {
+                array[i] = Convert.ToInt32(data[i]);
+            }
+        }
+    }
+    static void SwapMinAndMax(in int n, in int[] array)
+    {
+        int minpos = 0, maxpos = 0, minval = array[0], maxval = array[0];
+        for (int i = 1; i < n; i++)
+        {
+            if (array[i] < minval)
+            {
+                minpos = i;
+                minval = array[i];
+            }
+            if (array[i] > maxval)
+            {
+                maxpos = i;
+                maxval = array[i];
+            }
+        }
+        (array[minpos], array[maxpos]) = (maxval, minval);
+    }
+    static async void Output(string locate, int[] array, int flag)
+    {
+        string? line = string.Join(" ", array);
+        if (flag == 2)
+            await File.WriteAllTextAsync(locate, line);
+        else
+        {
+            Console.Write("Вихідний масив:");
+            Console.WriteLine(line);
+        }
+    }
+    
 }
